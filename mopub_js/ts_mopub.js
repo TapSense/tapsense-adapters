ce895fce11a8895105f76e78069d2011a2127aa1
crystalt <crystalytse@gmail.com>
2014-07-28 14:48:30 -0700

    Updated MoPub JS file to version 1.0.4: - Removed null check for ts_pub and ts_app - Cleaned up code

--- mopub_js/ts_mopub.js
+++ mopub_js/ts_mopub.js
@@ -1,12 +1,14 @@
 var TS_SERVER_HOST = "http://ads04.tapsense.com/ads/mopubad";
-var TS_VERSION = "1.0.3";
+var TS_VERSION = "1.0.4";
 
 var ts_click_tracker;
 var paramMap = {};
 var cookie_name = "ts-sesssion-cookie";
 
 function addParameter(key, value) {
-    paramMap[key] = value;
+    if (value) {
+        paramMap[key] = value;
+    }
 }
 
 function getSerializedParams() {
@@ -53,25 +55,21 @@ function getServerUrl() {
     addParameter("app", window.ts_app);
     addParameter("ufid", unique_function_id);
     addParameter("refer", window.location);
-    addParameter("size", window.ts_width + "x" + window.ts_height);
+    if (window.ts_width && window.ts_height) {
+        addParameter("size", window.ts_width + "x" + window.ts_height);
+    }
     addParameter("keywords", window.ts_keywords);
     addParameter("version", TS_VERSION);
     addParameter("jsonp", 1);
     addParameter("callback", "ts_callback")
-    if (window.ts_test) {
-        addParameter("test", window.ts_test);
-    }
+    addParameter("test", window.ts_test);
     if (window.ts_device_id) {
         addParameter("user", window.ts_device_id);
     } else {
         addParameter("session_id", getSession());
     }
-    if (window.ts_banner_ad) {
-        addParameter("banner_ad", window.ts_banner_ad);
-    }
-    if (window.ts_video_ad) {
-        addParameter("video_ad", window.ts_video_ad);
-    }
+    addParameter("banner_ad", window.ts_banner_ad);
+    addParameter("video_ad", window.ts_video_ad);
     if (typeof mopubFinishLoad == 'function') {
         addParameter("mr", 0);
     } else {
@@ -124,17 +122,6 @@ function getSession() {
 }
 
 (function () {
-    if (window.ts_ad_unit_id == null || window.ts_pub == null || window.ts_app == null) {
-        console.log("Failed to load ts ad. Missing 'ts_ad_unit_id' or 'ts_pub' or 'ts_app'");
-        mp_fail();
-        return;
-    }
-    if (window.ts_width == null) {
-        window.ts_width = 320;
-    }
-    if (window.ts_height == null) {
-        window.ts_height = 480;
-    }
     console.log('<script type="text/javascript" src="' + getServerUrl() + '"></s' + 'cript>');
     document.write('<script type="text/javascript" src="' + getServerUrl() + '"></s' + 'cript>');
 })();
